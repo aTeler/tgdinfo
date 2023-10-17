630dc1af4adf0c7ee6ea28ce3393061a88304b44
aTeler <105244128+aTeler@users.noreply.github.com>
2023-10-17 12:07:13 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
 // menu con javascript
 const menu = document.querySelector('#menu');
-menu.innerHTML=' <ul>    <h2> <img src="./img/comentario-alt.png" width="20" height="20"> INFO - Encuestas de satisfacción</h2>         <a href="clientes.html"><button class="boton">PRODUCTOS</button>  </a>         <a href="clientes.html"><button class="boton">CLIENTES</button></a>         <a href="formulario.html"><button class="boton">CONTACTO</button> </a>         <a href="login.html"><button class="boton">ACCESO</button>  </a>          </ul> ';
+//menu.innerHTML=' <ul>    <h2> <img src="./img/comentario-alt.png" width="20" height="20"> INFO - Encuestas de satisfacción</h2>         <a href="clientes.html"><button class="boton">PRODUCTOS</button>  </a>         <a href="clientes.html"><button class="boton">CLIENTES</button></a>         <a href="formulario.html"><button class="boton">CONTACTO</button> </a>         <a href="login.html"><button class="boton">ACCESO</button>  </a>          </ul> ';
+
+menu.innerHTML='<a href="index.html"> <i class="fa fa-phone"></i> INFO</a>  <a href="clientes.html">PRODUCTOS  </a>         <a href="clientes.html">CLIENTES</a>         <a href="formulario.html">CONTACTO </a>         <a href="login.html"><i class="fa fa-user"></i> ACCESO  </a> ';
--- js/pie.js
+++ js/pie.js
@@ -1,2 +1,4 @@
 //document.getElementById("pie").innerHTML = " Sistemas DGT - 2023 v1.0";
-document.getElementsByTagName("pie").innerHTML = "Sistemas DGT - 2023 v1.0";
+//document.getElementsByTagName("pie").innerHTML = "Sistemas DGT - 2023 v1.0";
+const pie = document.querySelector('#pie');
+pie.innerHTML='Sistemas Di Angelo-Giangiulio-Montes Güemez-Teler v1.0 -2023';
