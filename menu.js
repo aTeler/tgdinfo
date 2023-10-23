903bc49f52218e877f8b12f6f75377288e49e791
aTeler <105244128+aTeler@users.noreply.github.com>
2023-10-23 18:55:35 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,5 @@
+// menu con javascript
+const menu = document.querySelector('#menu');
+//menu.innerHTML=' <ul>    <h2> <img src="./img/comentario-alt.png" width="20" height="20"> INFO - Encuestas de satisfacción</h2>         <a href="clientes.html"><button class="boton">PRODUCTOS</button>  </a>         <a href="clientes.html"><button class="boton">CLIENTES</button></a>         <a href="formulario.html"><button class="boton">CONTACTO</button> </a>         <a href="login.html"><button class="boton">ACCESO</button>  </a>          </ul> ';
+
+menu.innerHTML='<a href="index.html"> <i class="fa fa-phone"></i> INFO</a>  <a href="precios.html">SERVICIOS</a>         <a href="clientes.html">CLIENTES</a>         <a href="formulario.html">CONTACTO </a>         <a href="login.html"><i class="fa fa-user"></i> ACCESO  </a> ';
