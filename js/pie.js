c0d280a1ac41e859dbff4b9d0e853968655d81d3
aTeler <105244128+aTeler@users.noreply.github.com>
2023-10-23 18:26:18 -0300

    Add files via upload

--- js/pie.js
+++ js/pie.js
@@ -1,4 +1,26 @@
 //document.getElementById("pie").innerHTML = " Sistemas DGT - 2023 v1.0";
 //document.getElementsByTagName("pie").innerHTML = "Sistemas DGT - 2023 v1.0";
 const pie = document.querySelector('#pie');
-pie.innerHTML='Sistemas Di Angelo-Giangiulio-Montes Güemez-Teler v1.0 -2023';
+pie.innerHTML='<div class="footercontenedor"><div class="footerlogo"> <img src="./img/logo.jpg"  alt="" width="120px" height="80px"> </div> <div class="foooterredes"> <a href="#" class="fa fa-facebook"> <a href="#" class="fa fa-twitter"> <a href="#" class="fa fa-instagram"></a>  <a href="#" class="fa fa-youtube"></a> </div> <div class="footertexto"> Sistema Survey Pack  desarollodado por Pedro Di Angelo, Facundo Giangiulio, Mariana Montes Güemez, Adrian Teler - info@surveypack.com.ar -  Codo a Codo 2023 </div></div>';
+
+
+/*<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-linkedin"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-pinterest"></a>
+<a href="#" class="fa fa-snapchat-ghost"></a>
+<a href="#" class="fa fa-skype"></a>
+<a href="#" class="fa fa-android"></a>
+<a href="#" class="fa fa-dribbble"></a>
+<a href="#" class="fa fa-vimeo"></a>
+<a href="#" class="fa fa-tumblr"></a>
+<a href="#" class="fa fa-vine"></a>
+<a href="#" class="fa fa-foursquare"></a>
+<a href="#" class="fa fa-stumbleupon"></a>
+<a href="#" class="fa fa-flickr"></a>
+<a href="#" class="fa fa-yahoo"></a>
+<a href="#" class="fa fa-reddit"></a>
+<a href="#" class="fa fa-rss"></a>*/
