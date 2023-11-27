009c7b160a1b336e697acb2c3a34e420e18eb6d7
aTeler <105244128+aTeler@users.noreply.github.com>
2023-11-27 20:09:27 -0300

    Add files via upload

---
+++ js/usuario.js
@@ -0,0 +1,81 @@
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        usuarios:[],
+        //url:'http://localhost:5000/usuarios', 
+   // si el backend esta corriendo local  usar localhost 5000(si no lo subieron a pythonanywhere)
+        url:'https://surveypack.pythonanywhere.com/usuarios',   // si ya lo subieron a pythonanywhere
+        error:false,
+        cargando:true,
+        /*atributos para el guardar los valores del formulario */ //usuario,clave,apellidos,nombres,mail,rol,fecha_alta,activo,empresa
+        id:0,
+        usuario:"", 
+        clave:"",
+        apellidos:"",
+        nombres:"",
+        mail:"",
+        rol:"",
+        fecha_alta:"",
+        activo:0,
+        empresa:0,
+    }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.usuarios = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(id) {
+            const url = this.url+'/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+			 alert('Registro Eliminado')
+                    location.reload(); // recarga el json luego de eliminado el registro
+                })
+        },
+        grabar(){  //usuario,clave,apellidos,nombres,mail,rol,fecha_alta,activo,empresa
+            let usuario = {
+                usuario:this.usuario,
+                clave: this.clave,
+                apellidos: this.apellidos,
+                nombres:this.nombres,
+                mail:this.mail,
+                rol:this.rol,
+                fecha_alta:this.fecha_alta,
+                activo:this.activo,
+                empresa:this.empresa
+            }
+            var options = {
+                body:JSON.stringify(usuario),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./usuarios.html";  // recarga usuarios.html
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")  // puedo mostrar el error tambien
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
