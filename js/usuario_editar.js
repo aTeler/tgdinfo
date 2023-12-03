15b6c84c09165976f4056217ef82e65a39a74e4b
aTeler <105244128+aTeler@users.noreply.github.com>
2023-12-03 18:35:22 -0300

    Add files via upload

---
+++ js/usuario_editar.js
@@ -0,0 +1,75 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // usuario_update.html?id=1
+console.log(id)
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        usuario:"", 
+        clave:"",
+        apellidos:"",
+        nombres:"",
+        mail:"",
+        rol:"",
+        fecha_alta:"",
+        activo:0,
+        empresa:0,
+        url:'https://surveypack.pythonanywhere.com/usuarios/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id
+                    this.usuario = data.usuario
+                    this.clave = data.clave
+                    this.apellidos=data.apellidos
+                    this.nombres=data.nombres
+                    this.mail=data.mail
+                    this.rol=data.rol
+                    this.fecha_alta=data.fecha_alta
+                    this.activo=data.activo
+                    this.empresa=data.empresa
+                
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let usuario = {
+                usuario:this.usuario,
+                clave: this.clave,
+                apellidos: this.apellidos,
+                mail: this.mail,
+                rol: this.rol,
+                fecha_alta: this.fecha_alta,
+                activo: this.activo,
+                empresa: this.empresa
+            }
+            var options = {
+                body: JSON.stringify(usuario),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./usuarios.html"; // navega a usuarios.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
